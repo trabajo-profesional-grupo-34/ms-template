a51005957ed55985184317b538208abd9ac5c8c7
Alex <garbieto@fi.uba.ar>
2024-03-02 19:10:50 -0300

    adding tests and auto pep

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "It's alive!"}
+
 
 @app.get("/hello/{name}")
 def hello_name(name: str = "World"):
